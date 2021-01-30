71cf20bd65541e88d8f9bd978b8cf24b71cad7d7
ChrisChrisLoLo <sporewoh@gmail.com>
2021-01-30 16:35:31 -0700

    take stab at models

--- back-end/api/quickstart/models.py
+++ back-end/api/quickstart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Author(models.Model):
+  user = models.OneToOneField(User,on_delete=models.CASCADE)
+  userType = 'Author'
+  displayName = models.CharField(max_length=50)
+  github = models.CharField(max_length=50)
+class Following(models.Model):
+  author = models.ForeignKey(Author)
+  followers = models.ManyToManyField(Author)
