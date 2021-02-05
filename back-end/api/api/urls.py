9472064fe2babf83ec61982fe5bb6585660aa911
Justin Mah <Jmmxp@users.noreply.github.com>
2021-02-05 15:56:16 -0700

    Change endpoint to /authors

--- back-end/api/api/urls.py
+++ back-end/api/api/urls.py
@@ -6,7 +6,7 @@ from quickstart import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register(r'author', views.AuthorViewSet)
+router.register(r'authors', views.AuthorViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
