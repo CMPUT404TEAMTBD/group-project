1086a41b3c1f25d184d5e42b45f5684fc5b0940d
Justin Mah <Jmmxp@users.noreply.github.com>
2021-02-05 18:28:07 -0700

    Fix spacing, add additional comment

--- back-end/api/quickstart/tests.py
+++ back-end/api/quickstart/tests.py
@@ -6,16 +6,16 @@ from .serializers import AuthorSerializer
 client = Client()
 
 class GetAuthorById(TestCase):
-  """Tests for getting a single Author by their ID.
+  """Tests for getting a single Author by their ID at endpoint /author/_id/.
   Referenced https://realpython.com/test-driven-development-of-a-django-restful-api/
   """
   def setUp(self):
-      self.john = Author.objects.create(
-        _id='testId', 
-        displayName='John Doe',
-        url="testUrl",
-        github="testGithub"
-      )
+    self.john = Author.objects.create(
+      _id='testId', 
+      displayName='John Doe',
+      url="testUrl",
+      github="testGithub"
+    )
 
   def test_get_valid_author(self):
     response = client.get(f'/authors/{self.john._id}/')
