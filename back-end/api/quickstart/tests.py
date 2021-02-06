1abbb4335cbac7272afc37cc6e5e97540de50048
Justin Mah <Jmmxp@users.noreply.github.com>
2021-02-06 15:21:05 -0700

    Add update valid/invalid post tests

--- back-end/api/quickstart/tests.py
+++ back-end/api/quickstart/tests.py
@@ -112,3 +112,76 @@ class GetPostById(TestCase):
   def test_get_invalid_post(self):
     response = client.get('/posts/invalidId/')
     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class UpdatePostById(TestCase):
+  """Tests for updating a single Author by PUT'ing to endpoint /author/_id/.
+  Referenced https://realpython.com/test-driven-development-of-a-django-restful-api/
+  """
+  def setUp(self):
+    self.post = Post.objects.create(
+      _id='123',
+      title='testpost',
+      description='i am a test post',
+      source='source post id',
+      origin='origin post id',
+      visibility='Public',
+      unlisted='True',
+      isPrivateToFriends='True',
+      author='testId',
+      contentType = 'text/plain',
+      content='Hello, I am a test post',
+      categories='["Testing"]',
+      published=datetime.datetime.now(),
+      count=5,
+      pageSize=20,
+      commentLink='link to comments',
+      comments='{ "text": "nice test" }'
+    )
+
+    self.payload = {
+      # TODO: Remove _id here once we figure out the _id field. It should be uneditable.
+      '_id': '123', 
+      'title': 'newpost', 
+      'description': 'i am a new post', 
+      'source': 'new source post id', 
+      'origin': 'new origin post id', 
+      'visibility': 'F', 
+      'unlisted': False, 
+      'isPrivateToFriends': False, 
+      'author': 'newId', 
+      'contentType': 'text/plain', 
+      'content': 'Hello, I am a new post', 
+      'categories': '["New Testing"]', 
+      'published': '22:11:27.254321', 
+      'count': 10, 
+      'pageSize': 40, 
+      'commentLink': 'new link to comments', 
+      'comments': '{ "text": "nice new test" }'
+    }
+
+  # TODO: update object tests most likely will be changed to use POST instead.
+  def test_update_post(self):
+    response = client.put(
+      f'/posts/{self.post._id}/',
+      data=json.dumps(self.payload),
+      content_type='application/json'
+    )
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # Compare each payload field with the updated Post object.
+    serializer = PostSerializer(Post.objects.get(_id=self.post._id))
+    for k in self.payload:
+      self.assertEqual(serializer.data[k], self.payload[k])
+
+    # Ensure other fields are unchanged
+    self.assertEqual(serializer.data['_id'], self.post._id)
+    self.assertEqual(serializer.data['_type'], self.post._type)
+
+  def test_update_invalid_post(self):
+    response = client.put(
+      '/posts/invalidId/',
+      data=json.dumps(self.payload),
+      content_type='application/json'
+    )
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
