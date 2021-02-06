ed621d92701155a0bdce69d94cdaad0d7f393b85
Justin Mah <Jmmxp@users.noreply.github.com>
2021-02-05 17:12:50 -0700

    Add GET author by _id tests

--- back-end/api/quickstart/tests.py
+++ back-end/api/quickstart/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework import status
+from django.test import TestCase, Client
+from .models import Author 
+from .serializers import AuthorSerializer
 
-# Create your tests here.
+client = Client()
+
+class GetAuthorById(TestCase):
+  """Tests for getting a single Author by their ID.
+  Referenced https://realpython.com/test-driven-development-of-a-django-restful-api/
+  """
+  def setUp(self):
+      self.john_doe = Author.objects.create(
+        _id='testId', 
+        displayName='John Doe',
+        url="testUrl",
+        github="testGithub"
+      )
+
+  def test_get_valid_author(self):
+    response = client.get('/authors/testId/')
+    author = Author.objects.get(_id=self.john_doe._id)
+    serializer = AuthorSerializer(author)
+    self.assertEqual(response.data, serializer.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+  def test_get_invalid_author(self):
+    response = client.get('/authors/invalidId/')
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
