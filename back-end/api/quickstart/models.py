3fb4eb943bda54446994a5715c5723c29f015ddd
VinnyLuu <36487188+VinnyLuu@users.noreply.github.com>
2021-02-05 20:09:33 -0700

    Implemented Post and getting of post with endpoint /posts/_id (#4)
    * created post model
    
    * setup post endpoint with get using id and update migrations
    
    * touched up models and added test
    
    * added links used
    
    * changed author to url
    
    * fixed url

--- back-end/api/quickstart/models.py
+++ back-end/api/quickstart/models.py
@@ -1,13 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+SHORT_CHAR_LENGTH = 32
+LONG_CHAR_LENGTH = 128
 
 # Create your models here.
 class Author(models.Model):
   # TODO: uncomment this when we want to use built in User model behaviour (probably for authorization)
   # user = models.OneToOneField(User,on_delete=models.CASCADE) 
-  _id = models.CharField(max_length=128, default='', unique=True)
+  _id = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
   _type = 'author'
-  displayName = models.CharField(max_length=32, default='')
-  url = models.CharField(max_length=128, default='', unique=True)
-  github = models.CharField(max_length=128, default='')
-  
+  displayName = models.CharField(max_length=SHORT_CHAR_LENGTH)
+  url = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
+  github = models.CharField(max_length=LONG_CHAR_LENGTH)
+
+
+class Post(models.Model):
+
+  class Visibility(models.TextChoices):
+    PUBLIC = 'P'
+    FRIENDS = 'F'
+
+  _id = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
+  _type = 'post'
+  title = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
+  description = models.TextField()
+  source = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
+  origin = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
+  visibility = models.CharField(max_length=SHORT_CHAR_LENGTH, choices=Visibility.choices)
+  unlisted = models.BooleanField()
+  isPrivateToFriends = models.BooleanField()
+  author = models.CharField(max_length=LONG_CHAR_LENGTH, unique=True)
+  contentType = models.CharField(max_length=LONG_CHAR_LENGTH)
+  content = models.TextField(blank=True)
+  categories = models.JSONField()
+  # cannot store time as IOS 8601 as per spec so when interacting will need parse (i.e using django.utils.dateparse)
+  published = models.TimeField(default=timezone.now)
+  count = models.IntegerField()
+  pageSize = models.IntegerField(default=50)
+  commentLink = models.CharField(max_length=LONG_CHAR_LENGTH)
+  # list of urls
+  comments = models.JSONField()
--- back-end/api/quickstart/tests.py
+++ back-end/api/quickstart/tests.py
@@ -1,9 +1,11 @@
 from rest_framework import status
 from django.test import TestCase, Client
-from .models import Author 
-from .serializers import AuthorSerializer
+from .models import Author, Post
+from .serializers import AuthorSerializer, PostSerializer
+import datetime
 
 client = Client()
+
 
 class GetAuthorById(TestCase):
   """Tests for getting a single Author by their ID at endpoint /author/_id/.
@@ -27,3 +29,39 @@ class GetAuthorById(TestCase):
   def test_get_invalid_author(self):
     response = client.get('/authors/invalidId/')
     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class GetPostById(TestCase):
+
+  def setUp(self):
+    self.post = Post.objects.create(
+      _id='123',
+      title='testpost',
+      description='i am a test post',
+      source='source post id',
+      origin='origin post id',
+      visibility='Public',
+      unlisted='True',
+      isPrivateToFriends='True',
+      author='testId',
+      contentType = 'text/plain',
+      content='Hello, I am a test post',
+      categories='["Testing"]',
+      published=datetime.datetime.now(),
+      count='5',
+      pageSize=20,
+      commentLink='link to comments',
+      comments='{ "text": "nice test" }'
+    )
+
+  def test_get_valid_post(self):
+    response = client.get(f'/posts/{self.post._id}/')
+    post = Post.objects.get(_id=self.post._id)
+    serializer = PostSerializer(post)
+    self.assertEqual(response.data, serializer.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_get_invalid_post(self):
+    response = client.get('/posts/invalidId/')
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
