e1997ea837e5b03774fc93349e3bfb84b5211526
Justin Mah <Jmmxp@users.noreply.github.com>
2021-02-06 19:25:52 -0700

    Bind ViewSets to URLs explicity to have POST be used for update functionality, fix tests, rename endpoints

--- back-end/api/api/urls.py
+++ back-end/api/api/urls.py
@@ -6,13 +6,26 @@ from quickstart import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register(r'authors', views.AuthorViewSet)
-router.register(r'posts', views.PostViewSet)
+
+author = views.AuthorViewSet.as_view({
+    'get': 'retrieve',
+    'post': 'update',
+    'put': 'create'
+})
+
+posts = views.PostViewSet.as_view({
+    'get': 'retrieve',
+    'post': 'update',
+    'delete': 'destroy',
+    'put': 'create'
+})
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('author/<str:_id>/', author, name='author'),
+    path('author/<str:author>/posts/<str:_id>/', posts, name='posts')
 ]
