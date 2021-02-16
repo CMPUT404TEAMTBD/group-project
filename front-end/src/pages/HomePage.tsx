5b702cf5b745fc2eebe36eb573745a45f3ed672a
ChrisChrisLoLo <sporewoh@gmail.com>
2021-02-16 01:59:11 -0700

    Front-end changes

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Row, Col } from 'reactstrap';
+import React, { useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { Row, Col, Form, Input } from 'reactstrap';
 
-export default function HomePage() {
+
+// https://stackoverflow.com/questions/44118060/react-router-dom-with-typescript/44571743
+export default function HomePage(props:RouteComponentProps) {
+
+  const [userSearch,setUserSearch] = useState('')
+
+  function onUserSearchChange(e:any){
+    setUserSearch(e.target.value);
+  }
+
+  function searchUsers(e:any){
+    e.preventDefault();
+    props.history.push('/authors/'+userSearch);
+  }
+
   return (
   <Row>
     <Col>
       <h3 className={"font-title"}>Home page :)</h3>
+      <Form inline={true} onSubmit={e => searchUsers(e)}>
+        <Input type="text" name="Author Search" placeholder="Search Authors" onChange={e => onUserSearchChange(e)}/>
+      </Form>
     </Col>
   </Row>
   );
