d8d8e5e91a29ec1f69bf92d2524f6ad14ff2d41e
Jmmxp <Jmmxp@users.noreply.github.com>
2021-02-24 19:40:09 -0700

    Add model tests (#78)

---
+++ back-end/api/quickstart/tests/models/test_comment.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from quickstart.models import Comment
+from types import SimpleNamespace
+from quickstart.tests.helper_test import get_test_comment_fields
+
+class CreateComment(TestCase):
+  """Tests for creating an comment."""
+  def setUp(self): 
+    self.fields = SimpleNamespace(**get_test_comment_fields())
+    self.comment = Comment.objects.create(**vars(self.fields), postId='testPostId')
+
+  def test_create_comment(self):
+    self.assertTrue(self.comment.id)
+    self.assertTrue(self.comment.postId)
+    self.assertTrue(self.comment.published)
+
+    self.assertEqual(self.comment.type, 'comment')
+    self.assertEqual(self.comment.author, self.fields.author)
+    self.assertEqual(self.comment.comment, self.fields.comment)
+    self.assertEqual(self.comment.contentType, self.fields.contentType)
