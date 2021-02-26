1caceb9ce74c000b235ba66dbc80b64dbde91708
Monica Uwalaka <muwalaka@ualberta.ca>
2021-02-25 17:45:35 -0700

    changed URI

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -24,7 +24,7 @@ export default function HomePage(props:RouteComponentProps) {
   function searchUsers(e:any){
     e.preventDefault();
     //props.history.push('/authors/'+userSearch);
-    axios.get('http://localhost:8000/api/author/' + userSearch + "/")
+    axios.get('process.env.REACT_APP_API_URL + "/api/author/' + userSearch + "/")
       .then(response => {
       console.log(response.data);
       setUserSearch(response.data);  
