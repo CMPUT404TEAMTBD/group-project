4edc5413f3c96bcdb318da39e546478838b9b505
Justin Mah <Jmmxp@users.noreply.github.com>
2021-02-25 19:07:04 -0700

    CharField to TextField

--- back-end/api/quickstart/models.py
+++ back-end/api/quickstart/models.py
@@ -3,19 +3,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-SHORT_CHAR_LENGTH = 32
-LONG_CHAR_LENGTH = 128
-
 # Create your models here.
 class Author(models.Model):
   # TODO: Null and blank for quicker testing purposes
   user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   type = 'author'
-  displayName = models.CharField(max_length=SHORT_CHAR_LENGTH)
+  displayName = models.TextField()  
   host = models.TextField()
   url = models.TextField()
-  github = models.CharField(max_length=LONG_CHAR_LENGTH)
+  github = models.TextField()
 
 
 class Post(models.Model):
@@ -30,10 +27,10 @@ class Post(models.Model):
   description = models.TextField()
   source = models.TextField(default='')
   origin = models.TextField(default='')
-  visibility = models.CharField(max_length=SHORT_CHAR_LENGTH, choices=Visibility.choices)
+  visibility = models.TextField(choices=Visibility.choices)
   unlisted = models.BooleanField()
   author = models.TextField(default='')
-  contentType = models.CharField(max_length=LONG_CHAR_LENGTH)
+  contentType = models.TextField()
   content = models.TextField(blank=True)
   categories = models.JSONField()
   # cannot store time as IOS 8601 as per spec so when interacting will need parse (i.e using django.utils.dateparse)
@@ -44,29 +41,29 @@ class Post(models.Model):
 class Comment(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   type = 'comment'
-  postId = models.CharField(max_length=LONG_CHAR_LENGTH)
-  author = models.CharField(max_length=LONG_CHAR_LENGTH)
+  postId = models.TextField()
+  author = models.TextField()
   comment = models.TextField()
-  contentType = models.CharField(max_length=LONG_CHAR_LENGTH)
+  contentType = models.TextField()
   published = models.DateTimeField(default=timezone.now, editable=False)
 
 
 class Follow(models.Model):
-  receiver = models.CharField(max_length=LONG_CHAR_LENGTH)
-  sender = models.CharField(max_length=LONG_CHAR_LENGTH)
+  receiver = models.TextField()
+  sender = models.TextField()
   approved = models.BooleanField()
 
 
 class Like(models.Model):
-  context = models.CharField(max_length=LONG_CHAR_LENGTH)
-  summary = models.CharField(max_length=LONG_CHAR_LENGTH)
+  context = models.TextField()
+  summary = models.TextField()
   type = 'like' 
-  author = models.CharField(max_length=LONG_CHAR_LENGTH)
-  object = models.CharField(max_length=LONG_CHAR_LENGTH)
+  author = models.TextField()
+  object = models.TextField()
   
 
 class Inbox(models.Model):
   type = 'inbox'
-  author = models.CharField(primary_key=True, max_length=LONG_CHAR_LENGTH)
+  author = models.TextField(primary_key=True)
   items = models.JSONField(default=list)
 
