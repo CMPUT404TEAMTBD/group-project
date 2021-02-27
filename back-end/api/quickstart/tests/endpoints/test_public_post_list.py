31aca000db9988a5943d1447ba3125d5997e85e1
Vinny Luu <vinnyluu2@gmail.com>
2021-02-26 22:02:16 -0700

    added default visibility for readability and removed post6 since its same as the first one

--- back-end/api/quickstart/tests/endpoints/test_public_post_list.py
+++ back-end/api/quickstart/tests/endpoints/test_public_post_list.py
@@ -14,22 +14,19 @@ class GetPublicPosts(TestCase):
     self.author2 = Author.objects.create(**get_test_author_fields())
 
     self.post1 = Post.objects.create(
-      **get_test_post_fields(1, visibility="Public"), author=self.author1
+      **get_test_post_fields(1, visibility="Public", unlisted=False), author=self.author1
     )
     self.post2 = Post.objects.create(
-      **get_test_post_fields(2, visibility="Public"), author=self.author2
+      **get_test_post_fields(2, visibility="Public", unlisted=False), author=self.author2
     )
     self.post3 = Post.objects.create(
-      **get_test_post_fields(3, visibility="Private"), author=self.author1
+      **get_test_post_fields(3, visibility="Private", unlisted=False), author=self.author1
     )
     self.post4 = Post.objects.create(
-      **get_test_post_fields(4, visibility="Private"), author=self.author2
+      **get_test_post_fields(4, visibility="Private", unlisted=False), author=self.author2
     )
     self.post5 = Post.objects.create(
       **get_test_post_fields(5, visibility="Public", unlisted=True), author=self.author1
-    )
-    self.post6 = Post.objects.create(
-      **get_test_post_fields(6, visibility="Public", unlisted=False), author=self.author1
     )
 
   def tests_get_all_public_posts(self):
@@ -42,4 +39,4 @@ class GetPublicPosts(TestCase):
     self.assertEqual(response.data, serializer.data)
     self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    self.assertEqual(len(response.data), 3)
+    self.assertEqual(len(response.data), 2)
