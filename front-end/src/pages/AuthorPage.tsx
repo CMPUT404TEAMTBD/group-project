1f98d5973f84c3b6ef78bf97b7245ea39507c08d
Raymond Sarinas <raysarinas97@gmail.com>
2021-02-28 19:58:57 -0700

    Add Stream to Author Profile Page (#92)
    * add route to author page
    
    * done author page
    
    * handle error
    
    * remove unnecessary comment/variable
    
    * refactor nav bar
    
    * Revert "refactor nav bar"
    
    This reverts commit eb8c38e59d73957b8d90543fcdf3225f0d97298e.
    
    * add stream to profile page
    
    * show posts in reverse chrono order
    
    * add comments and use author interface instead
    
    * make posts editable on profile page
    
    * finish comment
    
    * change while stmt and remove some comments

--- front-end/src/pages/AuthorPage.tsx
+++ front-end/src/pages/AuthorPage.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { UserLogin } from '../types/UserLogin';
+import { Post } from '../types/Post';
 import {
   Col,
   Container,
@@ -14,28 +15,78 @@ import {
   CardText,
   CardLink,
 } from 'reactstrap';
+import PostListItem from '../components/PostListItem';
+import { Author } from '../types/Author';
 
 interface Props {
   loggedInUser: UserLogin | undefined;
 }
 
-// https://react-bootstrap.github.io/layout/grid/
+/*
+  AuthorPage will render and display an author's profile - this includes information
+  about their user account and all the posts they have made
+*/
 export default function AuthorPage(props: Props) {
+  // if logged in, assign authorUrl
   const authorId = props.loggedInUser ? props.loggedInUser.authorId + "/" : "";
   const authorUrl = process.env.REACT_APP_API_URL + "/api/author/" + authorId;
-  const initialInputState = { displayName: undefined, github: undefined };
-  const [author, setAuthor] = useState(initialInputState);
+  const [author, setAuthor] = useState<Author|undefined>(undefined);
   const [responseMessage, setResponseMessage] = useState(100);
+  const [postEntries, setPostEntries] = useState<Post[] | undefined>(undefined);
 
+  // After clicking the profile navlink, get the appropriate author info and data
   useEffect(() => {
     axios.get(authorUrl).then(res => {
-      setAuthor(res.data);
+      const authorOb: Author = res.data;
+      setAuthor(authorOb);
       setResponseMessage(200);
     }).catch(err => {
       console.log("ERROR GETTING AUTHOR INFO");
       setResponseMessage(500);
     })
+
+    axios.get(process.env.REACT_APP_API_URL + "/api/author/" + authorId + "posts/",
+      {
+        auth: { // authenticate the GET request
+          username: props.loggedInUser ? props.loggedInUser.username : "",
+          password: props.loggedInUser ? props.loggedInUser.password : "",
+        }
+      }
+    ).then(res => {
+      const posts: Post[] = res.data;
+      const orderedPosts = posts.reverse();
+      setPostEntries(orderedPosts);
+    }).catch(err => {
+      console.log("ERROR GETTING POSTS");
+      setResponseMessage(500);
+    })
   }, []);
+
+  /* display a card to show posts loading, otherwise show that the user either has
+    no posts or display all posts */
+  const postCards = () => {
+    if (!postEntries) {
+      return (
+        <Card>
+          <CardBody>
+            <CardTitle tag="h5">LOADING POSTS</CardTitle>
+          </CardBody>
+        </Card>
+      )
+    }
+    if (postEntries.length == 0) {
+      return (
+        <Card>
+          <CardBody>
+            <CardTitle tag="h5">No Posts!</CardTitle>
+          </CardBody>
+        </Card>
+      )
+    }
+    return (
+      postEntries.map((post: Post) => <PostListItem post={post} key={post.id} isEditable={true} loggedInUser={props.loggedInUser} />)
+    )
+  };
 
   if (responseMessage > 299) {
     return (<Container>
@@ -45,25 +96,19 @@ export default function AuthorPage(props: Props) {
 
   return (
     <Container fluid>
-      <Row>
-        <Col sm={3}>
-          <Card>
-            {/* TODO: maybe add profile pic? uncomment line below */}
+      <Row className="justify-content-md-center">
+        <Col sm={2}>
+          <Card body className="text-center">
+            {/* TODO: maybe add profile pic. uncomment line below */}
             {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
             <CardBody>
-              <CardTitle tag="h5">{author.displayName}</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">@{props.loggedInUser?.username}</CardSubtitle>
+              <CardTitle tag="h5">{author?.displayName}</CardTitle>
               <CardLink href={author ? author.github : "#"} >GitHub</CardLink>
             </CardBody>
           </Card>
         </Col>
-        <Col sm={9}>
-          <Card>
-            {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
-            <CardBody>
-              <CardTitle tag="h5">RENDER POSTS IN THIS CARD</CardTitle>
-            </CardBody>
-          </Card>
+        <Col sm={5}>
+          {postCards()}
         </Col>
       </Row>
     </Container>
