b8d0c600786c92ed0a7f76654826f37ff1e0ee15
raysarinas <raysarinas97@gmail.com>
2021-02-28 18:11:34 -0700

    add comments and use author interface instead

--- front-end/src/pages/AuthorPage.tsx
+++ front-end/src/pages/AuthorPage.tsx
@@ -16,23 +16,30 @@ import {
   CardLink,
 } from 'reactstrap';
 import PostListItem from '../components/PostListItem';
+import { Author } from '../types/Author';
 
 interface Props {
   loggedInUser: UserLogin | undefined;
 }
 
 // https://react-bootstrap.github.io/layout/grid/
+/*
+  AuthorPage will render and display an author's profile - this includes information
+  about their user account and 
+*/
 export default function AuthorPage(props: Props) {
+  // if logged in, assign authorUrl
   const authorId = props.loggedInUser ? props.loggedInUser.authorId + "/" : "";
   const authorUrl = process.env.REACT_APP_API_URL + "/api/author/" + authorId;
-  const initialInputState = { displayName: undefined, github: undefined };
-  const [author, setAuthor] = useState(initialInputState);
+  const [author, setAuthor] = useState<Author|undefined>(undefined);
   const [responseMessage, setResponseMessage] = useState(100);
   const [postEntries, setPostEntries] = useState<Post[] | undefined>(undefined);
 
+  // After clicking the profile navlink, get the appropriate author info and data
   useEffect(() => {
     axios.get(authorUrl).then(res => {
-      setAuthor(res.data);
+      const authorOb: Author = res.data;
+      setAuthor(authorOb);
       setResponseMessage(200);
     }).catch(err => {
       console.log("ERROR GETTING AUTHOR INFO");
@@ -41,7 +48,7 @@ export default function AuthorPage(props: Props) {
 
     axios.get(process.env.REACT_APP_API_URL + "/api/author/" + authorId + "posts/",
       {
-        auth: {
+        auth: { // authenticate the GET request
           username: props.loggedInUser ? props.loggedInUser.username : "",
           password: props.loggedInUser ? props.loggedInUser.password : "",
         }
@@ -56,6 +63,8 @@ export default function AuthorPage(props: Props) {
     })
   }, []);
 
+  /* display a card to show posts loading, otherwise show that the user either has
+    no posts or display all posts */
   const postCards = () => {
     while (!postEntries) {
       return (
@@ -94,8 +103,9 @@ export default function AuthorPage(props: Props) {
             {/* TODO: maybe add profile pic. uncomment line below */}
             {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
             <CardBody>
-              <CardTitle tag="h5">{author.displayName}</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">@{props.loggedInUser?.username}</CardSubtitle>
+              <CardTitle tag="h5">{author?.displayName}</CardTitle>
+              {/* TODO: decide if we want to display/differentiate between username and display name. Results may vary across servers */}
+              {/* <CardSubtitle tag="h6" className="mb-2 text-muted">@{props.loggedInUser?.username}</CardSubtitle> */}
               <CardLink href={author ? author.github : "#"} >GitHub</CardLink>
             </CardBody>
           </Card>
