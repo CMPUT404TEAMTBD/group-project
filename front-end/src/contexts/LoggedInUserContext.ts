c47174b26a565d48691022b1722bf06b65b0da75
raysarinas <raysarinas97@gmail.com>
2021-03-04 20:48:55 -0700

    add comment to LoggedInUserContext

--- front-end/src/contexts/LoggedInUserContext.ts
+++ front-end/src/contexts/LoggedInUserContext.ts
@@ -1,6 +1,9 @@
 import React from "react";
 import { UserLogin } from "../types/UserLogin";
 
+/**
+ * Allows us to globally access the logged in user instead of having to manually pass it through props
+ */
 export const LoggedInUserContext = React.createContext<undefined|UserLogin>(
   undefined
 );
