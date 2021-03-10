d8f0c10e1d5707924eed2fd11369a8f5adefa4f3
ChrisChrisLoLo <sporewoh@gmail.com>
2021-03-10 14:24:24 -0700

    test again

--- back-end/api/api/settings.py
+++ back-end/api/api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 import django_on_heroku
 
 from pathlib import Path
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {    
