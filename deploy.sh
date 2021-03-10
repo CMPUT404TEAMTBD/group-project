a9213e779525cf4bf3fdfa9a0d1795b02e553ca7
ChrisChrisLoLo <sporewoh@gmail.com>
2021-03-10 13:19:52 -0700

    try again

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 heroku create qonnectionz
-# heroku buildpacks:add --index 1 heroku/nodejs
-heroku buildpacks:add --index 1 heroku/python
+heroku buildpacks:clear
+# from tima anovsky, https://medium.com/@timanovsky/heroku-buildpack-to-support-deployment-from-subdirectory-e743c2c838dd
+heroku buildpacks:set https://github.com/timanovsky/subdir-heroku-buildpack
+heroku buildpacks:add heroku/python
 heroku addons:create heroku-postgresql:hobby-dev
+heroku config:set PROJECT_PATH=back-end
 git push heroku main
