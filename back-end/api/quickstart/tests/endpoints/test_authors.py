cff09c4a620afd680a05326320399608c81de31f
Justin Mah <Jmmxp@users.noreply.github.com>
2021-03-20 15:45:43 -0600

    Match authors/ endpoint with other teams, return just a JSON array of authors.

---
+++ back-end/api/quickstart/tests/endpoints/test_authors.py
@@ -0,0 +1,27 @@
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from quickstart.models import Author
+from quickstart.serializers import AuthorSerializer
+from quickstart.tests.helper_test import get_test_author_fields
+
+client = Client()
+
+class GetAuthorById(TestCase):
+  """Tests for getting all Authors at endpoint /api/authors/."""
+  def test_get_authors(self):
+    for i in range(3):
+      Author.objects.create(**get_test_author_fields())
+
+    response = client.get(f'/api/authors/')
+    authors = Author.objects.all()
+    serializer = AuthorSerializer(authors, many=True)
+
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data, serializer.data)
+
+  def test_get_authors_empty(self):
+    response = client.get(f'/api/authors/')
+
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(len(response.data), 0)
