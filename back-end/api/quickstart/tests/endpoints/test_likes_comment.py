bf883762064ce5f2316a57c52ad5c62851efe181
Justin Mah <Jmmxp@users.noreply.github.com>
2021-03-19 22:56:50 -0600

    Fix likes by finding objects by checking contain substring instead of exact match

--- back-end/api/quickstart/tests/endpoints/test_likes_comment.py
+++ back-end/api/quickstart/tests/endpoints/test_likes_comment.py
@@ -20,7 +20,7 @@ class GetLikesForComment(TestCase):
   def test_get_likes_for_comment(self):
     response = client.get(f'/api/author/authorId/posts/postId/comments/{self.test_comment_id}/likes')
 
-    likes = Like.objects.filter(object=self.test_comment_id)
+    likes = Like.objects.filter(object__contains=self.test_comment_id)
     serializer = LikeSerializer(likes, many=True)
 
     self.assertEqual(response.status_code, status.HTTP_200_OK)
