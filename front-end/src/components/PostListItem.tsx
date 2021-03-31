451866f7114d2ab3cd02e449a95140fd982cb001
VinnyLuu <36487188+VinnyLuu@users.noreply.github.com>
2021-03-31 16:59:04 -0600

    added trailing slash to like object url (#202)
    * added trailing slash
    
    * fixed merge

--- front-end/src/components/PostListItem.tsx
+++ front-end/src/components/PostListItem.tsx
@@ -58,7 +58,7 @@ export default function PostListItem(props: Props) {
         const like: Like = {
           type: 'like',
           author: author,
-          object: `${props.post.author.host}api/author/${props.post.author.id}/posts/${props.post.id}`
+          object: `${props.post.author.host}api/author/${props.post.author.id}/posts/${props.post.id}/`
         }
 
         return AxiosWrapper.post(`${props.post.author.host}api/author/${props.post.author.id}/inbox/`,
