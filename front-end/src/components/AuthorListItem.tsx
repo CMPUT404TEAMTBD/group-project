24ad5ba6e5fb4853f2ce69993951033e8a0e6767
ChrisChrisLoLo <sporewoh@gmail.com>
2021-04-01 14:02:11 -0600

    Add default image for profiles

--- front-end/src/components/AuthorListItem.tsx
+++ front-end/src/components/AuthorListItem.tsx
@@ -5,6 +5,7 @@ import { Card, CardBody, Container, Row, Col, CardImg, CardTitle, Button, CardLi
 import { Author } from "../types/Author";
 import { UserLogin } from '../types/UserLogin';
 import FollowRequestButton from './FriendRequestButton';
+import { isValidGithub } from '../helpers/GithubHelper';
 
 interface Props {
   author: Author;
@@ -43,7 +44,12 @@ export default function AuthorListItem(props: Props) {
       <Container fluid>
         <Row>
           <Col xs="2">
-            <CardImg top src={props.author.github + ".png"} alt="Profile Pic" />
+            { !isValidGithub(props.author.github) ?
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+              </svg>:
+              <CardImg top src={props.author.github + ".png"} alt="Profile Pic" />
+            }
           </Col>
           <Col xs="6">
             <CardTitle tag="h3" ><Link to={{ pathname: `/author/${props.author.id}` }}>{props.author.displayName}</Link></CardTitle>
--- front-end/src/pages/AuthorPage.tsx
+++ front-end/src/pages/AuthorPage.tsx
@@ -70,9 +70,11 @@ export default function AuthorPage(props: any) {
   const profilePic = () => {
     if (author?.github?.includes("github.com")) {
       return <CardImg top width="100%" src={author.github + ".png"} alt="Card image cap" />
+    } else {
+      return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+              </svg>
     }
-
-    return null
   }
 
   if (responseMessage > 299) {
