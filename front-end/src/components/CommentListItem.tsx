9d6f1d2ec332f6f1fe3e94a284f26bc03b33dac8
VinnyLuu <36487188+VinnyLuu@users.noreply.github.com>
2021-04-06 21:06:18 -0600

    fix indentation from last pr (#217)

--- front-end/src/components/CommentListItem.tsx
+++ front-end/src/components/CommentListItem.tsx
@@ -8,61 +8,61 @@ import { Like } from '../types/Like'
 import { likeButtonIcon, likedButtonIcon } from '../assets/Icons';
 
 interface Props {
-    postId: string,
-    postAuthor: Author,
-    comment: PostComment,
-    loggedInUser: UserLogin | undefined,
+  postId: string,
+  postAuthor: Author,
+  comment: PostComment,
+  loggedInUser: UserLogin | undefined,
 }
 
 export default function CommentListItem(props: Props) {    
-    const [likes, setLikes] = useState<Like[]>([]);
-    const [hasLiked, setHasLiked] = useState<boolean>(false);
+  const [likes, setLikes] = useState<Like[]>([]);
+  const [hasLiked, setHasLiked] = useState<boolean>(false);
 
-    useEffect(() => {
-      AxiosWrapper.get(`${props.comment.author.host}api/author/${props.postAuthor.id}/posts/${props.postId}/comments/${props.comment.id}/likes/`, props.loggedInUser).then((res: any) => {
-        const resLikes: Like[] = res.data.items;
-        setLikes(resLikes);
-        setHasLiked(resLikes.filter((l: Like) => l.author.id === props.loggedInUser?.authorId).length !== 0);
-      })
-    }, [hasLiked, props.comment.author.host, props.comment.id, props.loggedInUser, props.postAuthor.id, props.postId]);
+  useEffect(() => {
+    AxiosWrapper.get(`${props.comment.author.host}api/author/${props.postAuthor.id}/posts/${props.postId}/comments/${props.comment.id}/likes/`, props.loggedInUser).then((res: any) => {
+      const resLikes: Like[] = res.data.items;
+      setLikes(resLikes);
+      setHasLiked(resLikes.filter((l: Like) => l.author.id === props.loggedInUser?.authorId).length !== 0);
+    })
+  }, [hasLiked, props.comment.author.host, props.comment.id, props.loggedInUser, props.postAuthor.id, props.postId]);
 
-    const LikeCardLink = () => props.loggedInUser
-    ? hasLiked
-      ? <CardLink>{likedButtonIcon}</CardLink>
-      : <CardLink onClick={() => likeComment()}>{likeButtonIcon}</CardLink>
-    : null;
+  const LikeCardLink = () => props.loggedInUser
+  ? hasLiked
+    ? <CardLink>{likedButtonIcon}</CardLink>
+    : <CardLink onClick={() => likeComment()}>{likeButtonIcon}</CardLink>
+  : null;
 
-    function likeComment(): void {
-      if (!props.loggedInUser) {
-        console.error("User is not logged in, cannot like!");
-        return;
-      }
-      AxiosWrapper.get(`${process.env.REACT_APP_API_URL}/api/auth-user/${props.loggedInUser.username}/`, props.loggedInUser)
-        .then((res: any) => {
-          const author: Author = res.data;
-          const like: Like = {
-            type: 'like',
-            author: author,
-            object: `${props.postAuthor.host}api/author/${props.postAuthor.id}/posts/${props.postId}/comment/${props.comment.id}`
-          }
-
-          return AxiosWrapper.post(`${props.comment.author.host}api/author/${props.comment.author.id}/inbox/`,
-            like, props.loggedInUser
-          )
-        }).then((res: any) => {
-          alert('You liked the comment!');
-          setHasLiked(true);
-        });
+  function likeComment(): void {
+    if (!props.loggedInUser) {
+      console.error("User is not logged in, cannot like!");
+      return;
     }
+    AxiosWrapper.get(`${process.env.REACT_APP_API_URL}/api/auth-user/${props.loggedInUser.username}/`, props.loggedInUser)
+      .then((res: any) => {
+        const author: Author = res.data;
+        const like: Like = {
+          type: 'like',
+          author: author,
+          object: `${props.postAuthor.host}api/author/${props.postAuthor.id}/posts/${props.postId}/comment/${props.comment.id}`
+        }
 
-    return (
-        <Card>
-            <CardHeader className="mb-2 text-muted"><b>@{props.comment.author.displayName}</b></CardHeader>
-            <CardBody>
-                {props.comment.comment}
-                <CardText>🔥{likes.length}</CardText>
-                {LikeCardLink()}
-            </CardBody>
-        </Card>
-    )
+        return AxiosWrapper.post(`${props.comment.author.host}api/author/${props.comment.author.id}/inbox/`,
+          like, props.loggedInUser
+        )
+      }).then((res: any) => {
+        alert('You liked the comment!');
+        setHasLiked(true);
+      });
+  }
+
+  return (
+      <Card>
+          <CardHeader className="mb-2 text-muted"><b>@{props.comment.author.displayName}</b></CardHeader>
+          <CardBody>
+              {props.comment.comment}
+              <CardText>🔥{likes.length}</CardText>
+              {LikeCardLink()}
+          </CardBody>
+      </Card>
+  )
 }
