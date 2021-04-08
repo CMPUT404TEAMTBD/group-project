73379b914d0cd49f2be84747cddd0c2f6dd7f189
VinnyLuu <36487188+VinnyLuu@users.noreply.github.com>
2021-04-07 23:13:33 -0600

    Frontend for comment pagination (#225)
    * added error chefrontend and error handling for comment pagination
    
    * changed to use icon instead

--- front-end/src/components/PostDetailModal.tsx
+++ front-end/src/components/PostDetailModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { CardLink, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { Post } from '../types/Post';
 import { PostComment } from '../types/PostComment';
 import { UserLogin } from '../types/UserLogin';
@@ -8,6 +8,7 @@ import CommentList from './CommentList';
 import PostContent from './PostContentEl';
 import { getDateString } from '../helpers/DateHelper';
 import { AxiosWrapper } from '../helpers/AxiosWrapper';
+import { chevronDoubleDown } from '../assets/Icons';
 
 interface Props {
   post: Post;
@@ -24,17 +25,34 @@ export default function PostDetailModal(props:Props) {
   const post: Post = props.post;
 
   const [commentList, setCommentList] = useState<PostComment[] | undefined>(undefined);
+  const [commentPageNum, setCommentPageNum] = useState(2);
+  const [noMoreComments, setNoMoreComments] = useState(false);
 
   function fetchComments() {
     if (props.loggedInUser !== undefined) {
       AxiosWrapper.get(`${post.author.url}posts/${post.id}/comments/`, props.loggedInUser).then((res: any) => {
         const comments: PostComment[] = res.data;
-        // Reverse the comments so that they are in order (from newest to oldest).
-        setCommentList(comments.reverse());
+        setCommentList(comments);
       }).catch((err: any) => {
         console.error(err);
-      });
+      }); 
     }
+  }
+
+  function addMoreComments() {
+    AxiosWrapper.get(`${post.author.url}posts/${post.id}/comments/?page=${commentPageNum}`, props.loggedInUser).then((res: any) => {
+      if (res.status === 204 || res.data.length < 5) {
+        setNoMoreComments(true);
+      }
+      if (commentList) {
+        setCommentList([...commentList, ...res.data])
+      } else {
+        setCommentList(res.data)
+      }
+      setCommentPageNum(commentPageNum + 1)
+    }).catch((err: any) => {
+      console.error(err);
+    });
   }
 
   useEffect(() => {
@@ -56,6 +74,7 @@ export default function PostDetailModal(props:Props) {
         <CommentFormElement loggedInUser={props.loggedInUser} postId={post.id} postAuthor={post.author} commentList={commentList} setCommentList={setCommentList} />
       </ModalFooter>
       {props.loggedInUser && <ModalFooter><CommentList loggedInUser={props.loggedInUser} postId={post.id} postAuthor={post.author} commentList={commentList} /></ModalFooter>}
+      {!noMoreComments ? <CardLink onClick={() => addMoreComments()} style={{paddingBottom: '1em', marginLeft: 'auto', marginRight: 'auto'}}>{chevronDoubleDown}</CardLink> : null}
     </Modal>
   );
 }
